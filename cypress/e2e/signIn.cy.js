cfa15ae39d92cf9315bcb0440730c4d97e49a4ce
Vitalii <v.hulaievych@gmail.com>
2026-02-25 19:30:59 +0000

    solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,16 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('/');
+    cy.contains('.nav-link', 'Sign in').should('exist');
 
+    cy.contains('.nav-link', 'Sign in').click();
+
+    cy.get('[placeholder=Email]').type('vitalii120296@gmail.com');
+    cy.get('[placeholder=Password]').type('vitalii120296');
+
+    cy.contains('.btn', 'Sign in').click();
+
+    cy.contains('a', 'conduit').should('exist');
   });
 });
