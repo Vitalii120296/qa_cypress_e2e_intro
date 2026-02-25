c8d2fbb956e1ddb09eac5094d85ee7d4ef6b523b
Vitalii <v.hulaievych@gmail.com>
2026-02-25 19:37:15 +0000

    solution

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,15 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-    }
+      on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.family === 'chromium') {
+          launchOptions.args.push('--blink-settings=imagesEnabled=false');
+        }
+        return launchOptions;
+      });
+    },
+    baseUrl: 'https://conduit.mate.academy/',
+    viewportWidth: 1024,
+    viewportHeight: 768
   }
 });
